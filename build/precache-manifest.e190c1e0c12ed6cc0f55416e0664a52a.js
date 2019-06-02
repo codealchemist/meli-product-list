5225259f53e3f1274489bf8c6e55dcfdefc2edfe
Alberto Miranda <codealchemist@gmail.com>
2019-06-01 21:51:58 -0300

    Moved dependencies to devDependencies; added serve and Procfile; added build to repo; will serve build.

---
+++ build/precache-manifest.e190c1e0c12ed6cc0f55416e0664a52a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6381624bc36e323da2230f4dcb9433c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a53d95ed24d4f5292075",
+    "url": "/static/css/main.65c5756a.chunk.css"
+  },
+  {
+    "revision": "303f924694a6c286a7e1",
+    "url": "/static/js/2.765383e6.chunk.js"
+  },
+  {
+    "revision": "a53d95ed24d4f5292075",
+    "url": "/static/js/main.72dbd199.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
